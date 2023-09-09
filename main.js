b06b8827a6d5ab71e72f72b9e75114db43586f11
Saidxojaakbarxojayev <134775160+Saidxojaakbarxojayev@users.noreply.github.com>
2023-09-09 16:16:26 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+let elSlide = document.querySelector(".slider")
+let btn1 = document.querySelector(".btn1")
+let btn2 = document.querySelector(".btn2")
+let btn3 = document.querySelector(".btn3")
+let btn4 = document.querySelector(".btn4")
+
+let text_one = document.createElement("p")
+text_one.innerText = "Saidxo'ja"
+document.body.appendChild(text_one)
